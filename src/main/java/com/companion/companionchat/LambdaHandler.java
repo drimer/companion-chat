ab62f2f2623247df1e2ce64a766d47858cd8c682
Alberto Aguilera Juarez <alberto.aguilera@xdesign.com>
2024-09-20 14:30:30 +0100

    feat: first working lambda in AWS

---
+++ src/main/java/com/companion/companionchat/LambdaHandler.java
@@ -0,0 +1,35 @@
+package com.companion.companionchat;
+
+
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class LambdaHandler implements RequestStreamHandler {
+    private static SpringBootLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getHttpApiV2ProxyHandler(CompanionChatApplication.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Could not initialize Spring Boot application", e);
+        }
+    }
+
+    @Override
+    public void handleRequest(
+        InputStream inputStream,
+        OutputStream outputStream,
+        Context context
+    )
+        throws IOException {
+        handler.proxyStream(inputStream, outputStream, context);
+    }
+}
