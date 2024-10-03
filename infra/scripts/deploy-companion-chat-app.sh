a34dfe79f1a3c5fed17f5d15b9d4a4aaacde9e02
Alberto Aguilera Juarez <alberto.aguilera@xdesign.com>
2024-10-03 13:43:05 +0100

    feat: try adding resource namespacing from pipeline for dev

---
+++ infra/scripts/deploy-companion-chat-app.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -x
+
+./gradlew clean build buildZip
+sam package --template-file infra/cc-app-stack.yml --output-template-file packaged.yml --s3-bucket cf-templates-1tytu2v0l6bsz-eu-west-2
+sam deploy --template-file packaged.yml --stack-name ${ENVIRONMENT}-companion-chat-stack --parameter-overrides Environment=${ENVIRONMENT} --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
