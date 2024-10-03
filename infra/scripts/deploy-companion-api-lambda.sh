62e627cab2d87aba023e740ce125a517f46c766d
Alberto Aguilera Juarez <alberto.aguilera@xdesign.com>
2024-10-03 11:47:10 +0100

    feat: add namespacing for dev

--- infra/scripts/deploy-companion-api-lambda.sh
+++ infra/scripts/deploy-companion-api-lambda.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 ./gradlew clean build buildZip
-sam package --template-file infra/cf-stack.yml --output-template-file packaged.yml --s3-bucket cf-templates-1tytu2v0l6bsz-eu-west-2
+sam package --template-file infra/cc-app-stack.yml --output-template-file packaged.yml --s3-bucket cf-templates-1tytu2v0l6bsz-eu-west-2
 sam deploy --template-file packaged.yml --stack-name companion-chat-stack --capabilities CAPABILITY_IAM CAPABILITY_AUTO_EXPAND
