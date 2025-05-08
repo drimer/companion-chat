88ace20681f3561e330c23a24257e45d8cb73516
Alberto Aguilera <drimer.aaj@gmail.com>
2025-05-08 16:34:46 +0100

    fix: don't use archive_file from terraform

--- infra/aws/modules/api/main.tf
+++ infra/aws/modules/api/main.tf
@@ -16,21 +16,12 @@ resource "aws_iam_role" "api_lambda_role" {
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
 }
 
-data "archive_file" "api_lambda_archive_file" {
-  type        = "zip"
-  source_file = "${path.module}/../../../../build/distributions/companion-chat-0.0.1-SNAPSHOT.zip"
-  output_path = "api_lambda_function_payload.zip"
-}
-
 resource "aws_lambda_function" "test_lambda" {
-#   filename      = "api_lambda_function_payload.zip"
-  filename = data.archive_file.api_lambda_archive_file.output_path
   function_name = join("-", compact(tolist([var.group, var.environment, var.scope, var.lambda_function_name])))
   role          = aws_iam_role.api_lambda_role.arn
   handler       = "com.companion.companionchat.LambdaHandler::handleRequest"
-
-  source_code_hash = data.archive_file.api_lambda_archive_file.output_base64sha256
-
+  filename = "${path.module}/../../../../build/distributions/companion-chat-0.0.1-SNAPSHOT.zip"
+  source_code_hash = filebase64sha256("${path.module}/../../../../build/distributions/companion-chat-0.0.1-SNAPSHOT.zip")
   runtime = var.lambda_function_runtime
   memory_size = 128
   timeout = 30
