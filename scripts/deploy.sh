4818557737da08cfe6183c399c1fb42cbf39e142
Alberto Aguilera <drimer.aaj@gmail.com>
2025-05-08 15:12:59 +0100

    fix: ensure terraform cli is installed in the pipeline

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x 
+
 terraform -chdir=infra/aws/environments/dev init
 terraform -chdir=infra/aws/environments/dev workspace select -or-create dev
 terraform -chdir=infra/aws/environments/dev plan -out=tfplan
