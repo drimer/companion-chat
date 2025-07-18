982183905d67a4f0c5502166e73ba4f12a70d75a
Alberto Aguilera <drimer.aaj@gmail.com>
2025-07-18 11:03:34 +0100

    fix: use correct table nam (still hardcoded though)

--- src/companionchat/db/repositories.py
+++ src/companionchat/db/repositories.py
@@ -13,7 +13,7 @@ class ConversationRepository:
         try:
             conversation_id = uuid4()
             await self.client.put_item(
-                TableName="companion-chat-dev-chat-chats-db",
+                TableName="companion-chat-dev-chat-conversations-db",
                 Item={
                     "id": {"S": str(conversation_id)},
                     "messages": {"L": []},
